bce89b254097b7825b3341ecd6cea2accd9ec6aa
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-05-02 13:28:13 +0100

    actualización

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,10 +2,17 @@ import "./Header.css";
 
 
 const templete = () => {
-    return `
-    
-    
-    `
+    return /*html*/ `
+    <div>
+        <img src="https://res.cloudinary.com/dwdznuzaz/image/upload/v1683025870/Games-Center-sin_fondo_f9z9bu.png" alt="icono de la web"/>
+        <h1>Hub Games</h1>
+    </div>
+    <nav>
+        <button id="buttonHome">Home</button>
+        <input id="checkitem" type="checkbox" onclick="enableDarkMode()"/>
+        <img src="https://res.cloudinary.com/dwdznuzaz/image/upload/v1683023350/SalirSesi%C3%B3n-removebg-preview_qaroec.png" alt="logout"/>
+    </nav>
+`
 };
 
 const addListeners = () => {
@@ -13,5 +20,6 @@ const addListeners = () => {
 };
 
 export const printTemplates = () => {
-    document.querySelector("header").innerHTML = templete
+    document.querySelector("header").innerHTML = templete() 
 } 
+
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import './style.css';
+import { printTemplates as printHeader} from './components/Header/Header';
 import { printTemplates as Pokemon} from './pages/Pokemon/Pokemon';
 import { printTemplates as Dashboard } from './pages/Dashboard/Dashboard';
 import { printTemplates as HangMan} from './pages/HangMan/HangMan';
 import { printTemplates as Login} from './pages/Login/Login';
 import { printTemplates as QuizPlay} from './pages/QuizPlay/QuizPlay';
+
 
 const app = document.querySelector("#app");
 const header = document.createElement("header");
@@ -11,8 +13,9 @@ const main = document.createElement("main");
 const footer = document.createElement("footer");
 
 app.append(header, main, footer);
+printHeader()
 
-export const initContent = (route) => {
+export const initControler = (route) => {
 
     switch (route) {
         case undefined:
@@ -39,6 +42,11 @@ export const initContent = (route) => {
     }
 };
 
-initContent();
+initControler();
 
 
+// función para pasar de modo claro a modo oscuro
+function enableDarkMode() {
+    let main_body = document.body;
+    main_body.classList.toggle("dark-mode");
+}
