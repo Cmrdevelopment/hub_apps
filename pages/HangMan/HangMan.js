2f703b3948d5f1326883d924fadb8487bcf8ad79
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-05-10 11:28:25 +0100

    actualización

--- pages/HangMan/HangMan.js
+++ pages/HangMan/HangMan.js
@@ -3,7 +3,7 @@ import "./HangMan.css";
 
 const templete = () => /*html*/`
     <section>
-    <h2>En estos momentos estamos mejorando el juego, en breve lo tendrás disponible con las mejoras</h2>
+    <h2>En estos momentos estamos mejorando el juego, en breve lo tendrás disponible con todas las mejoras disponibles</h2>
     <img src="https://res.cloudinary.com/dwdznuzaz/image/upload/v1683657019/Ahorcado_cerca_axvdhn.png" alt="imagen del juego del ahorcado">
     </section>
 `;
--- pages/Wacka/Wacka.js
+++ pages/Wacka/Wacka.js
@@ -1,6 +1,8 @@
 import "./Wacka.css";
 import { ramdomTime } from "../../utils/randomTime";
 
+//? ------------------ TEMPLATE GENERAL ---------------------------
+
 const templete = () => /*html*/`
     <div class="interface">
         <h1>Wacka</h1>
@@ -16,8 +18,6 @@ const templete = () => /*html*/`
         </div>
         <div class="hole hole3">
             <div clase="mole"></div>
-
-
         </div>
         <div class="hole hole4">
             <div clase="mole"></div>
@@ -31,13 +31,18 @@ const templete = () => /*html*/`
     </div>
 `;
 
+//? ------------------ VARIABLES GLOBALES ---------------------------
 
 let lastHole;
 let score = 0;
 let timeUp = false;
 
+//? ------------------ LÓGICA DEL JUEGO ---------------------------
+
+//!--> Función para que el topo que se vaya a asomar sea aleatorio
+
 const randomHole = () => {
-    //Index aleatorio para que salga de forma ramdon el topo
+    //Index aleatorio
     const index = Math.floor(
       Math.random() * document.querySelectorAll(".hole").length
     );
@@ -51,31 +56,56 @@ const randomHole = () => {
     return hole;
 };
 
+//!--> Función que decide que topo se asomará
 
 const showMole = () => {
     //Definimos el tiempo que el topo se mantiene asomado
     const time = ramdomTime(500, 1000);
-
     const hole = randomHole(document.querySelectorAll(".hole"));
     hole.classList.add("up");
-
     //SetTimeout para elimimar la clase
     setTimeout(() => {
         hole.classList.remove("up");
-
     // Siempre que el tiempo de la partida no haya acabado seguiremos asomando topos
-    if (!timeUp) {
+        if (!timeUp) {
         showMole();
-    }
-}, time);
-
+        }
+    }, time);
 }
 
-const addListeners = () => {
+//!--> Función que arranca el juego
+const startGame = () => {
 
+    timeUp = false;
+    score = 0;
+    showMole();
+
+    setTimeout(() => {
+
+        timeUp = true;
+    }, 15000);
 };
+
+//! --> Función que se ejecuta al pulsar el topo : suma puntuación y borra la clase al padre del evento
+const wack = (e) => {
+    console.log("pulso a topo");
+    score++;
+    e.target.parentNode.classList.remove("up");
+    document.querySelector(".score").textContent = score;
+};
+
+//? ------------------ EVENTOS DE LOS ELEMENTOS HTML ---------------------------
+
+const addListeners = () => {
+  //Añadimos a los topos el escuchador del click para saber cuando han clicado
+    document.querySelectorAll(".mole")
+    .forEach((mole) => mole.addEventListener("click", (e) => wack(e)));
+        document.querySelector("#startGame").addEventListener("click", startGame);
+};
+
+//?--------------- PINTAR EL CONTENEDOR GENERAL---------------------
 
 export const printTemplates = () => {
     document.querySelector("main").innerHTML = templete();
-    showMole();
+    addListeners();
 }
