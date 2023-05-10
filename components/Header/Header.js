47b5ee4afff644a258152e974d2e480ae2f59ebc
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-05-10 16:09:55 +0100

    actualización

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,4 +1,4 @@
-import { enableDarkMode } from "../../utils/changeColor";
+import { darkColor } from "../../utils/changeColor";
 import { initControler } from "../../utils/route";
 import "./Header.css";
 
@@ -10,8 +10,10 @@ const templete = () => {
         <h1>Hub Games</h1>
     </div>
     <nav>
-        <img id= "buttonDashboard" class="navDashboard" src="https://res.cloudinary.com/dwdznuzaz/image/upload/v1683039413/Mando_Dashboard_-removebg-preview_ilzorz.png" alt="ir al home"/> 
-        <input id="checkitem" type="checkbox" onclick="enableDarkMode()"/>
+        <img id= "buttonDashboard" class="navDashboard" src="https://res.cloudinary.com/dwdznuzaz/image/upload/v1683039413/Mando_Dashboard_-removebg-preview_ilzorz.png" alt="ir al home"/>
+        
+        <img id="changeColor" class="navChangeColor" src="https://res.cloudinary.com/dwdznuzaz/image/upload/v1683726939/lunasol_png-transparent-computer-icons-moon-symbol-sun-monochrome-black-silhouette-removebg-preview_dvi3ss.png"alt=" cambio a modo oscuro/claro"/>
+        
         <img id= "buttonlogout" class="navLogout" src="https://res.cloudinary.com/dwdznuzaz/image/upload/v1683023350/SalirSesi%C3%B3n-removebg-preview_qaroec.png" alt="logout"/>
     </nav>
 `
@@ -27,7 +29,11 @@ const addListeners = () => {
         localStorage.removeItem("user");
         initControler("Login")
         if (!localStorage.getItem("user")) document.querySelector("nav").style.display = "none";
-    });    
+    });
+    const buttonChangeColor = document.querySelector("#changeColor");
+    buttonChangeColor.addEventListener("click", () => {
+    darkColor()
+    });
 };
 
 export const printTemplates = () => {
